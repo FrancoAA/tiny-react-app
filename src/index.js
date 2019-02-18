22fbc20de144c9014419c41bb4b01d23ce3f76b4
Franco Alarcon <franco.alarcon@attackiq.com>
2019-02-17 21:17:31 -0300

    renames paths

--- src/index.js
+++ src/index.js
@@ -51,16 +51,16 @@ const MyComponent = () => (
   <Router>
     <div>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
+        <li><Link to="/reactjs">Home</Link></li>
+        <li><Link to="/reactjs/about">About</Link></li>
+        <li><Link to="/reactjs/topics">Topics</Link></li>
       </ul>
 
       <hr/>
 
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/topics" component={Topics}/>
+      <Route exact path="/reactjs" component={Home}/>
+      <Route path="/reactjs/about" component={About}/>
+      <Route path="/reactjs/topics" component={Topics}/>
     </div>
   </Router>
 );
